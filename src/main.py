80c66e789e0bee5a5eef49ee7f52be78b9c631af
Navid Hariri <navidhariri25@gmail.com>
2022-09-10 01:01:01 -0700

    fixed styling for success_screen.html with the new window size

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import eel
 eel.init("web")
 
 eel.start(
-    'templates/start_screen.html',
+    'templates/success_screen.html',
     jinja_templates='templates',
-    size=(800, 800),
+    size=(800, 850),
 )
