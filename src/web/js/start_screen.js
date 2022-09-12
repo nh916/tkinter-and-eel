eb7d382d315285b3ef60aa59053431874810ee35
Navid Hariri <navidhariri25@gmail.com>
2022-09-12 15:46:18 -0700

    starting to add some js functionality
    base.html: added an empty block for inputting js script for each screen
    
    start_screen.html: put the elements inside a form and added event handling on submit
    
    start_screen.js: started implementation for form submission

---
+++ src/web/js/start_screen.js
@@ -0,0 +1,17 @@
+/*
+document.querySelector("#auth_form").addEventListener("submit",
+    function (event) {
+//    TODO validate token
+//    after validation, pass to excel uploader to validate and if errors then output the errors
+//    hand over the data in json from js to eel python and then to excel uploader
+        console.log("hello world!");
+        // console.log(event);
+        event.preventDefault();
+        return false;
+    }
+);
+*/
+
+function runHere(event) {
+    console.log("hello world!");
+}
