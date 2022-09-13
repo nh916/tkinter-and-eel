8946d5699fc9b04294f156b571467a46890e7d29
Navid Hariri <navidhariri25@gmail.com>
2022-09-12 18:19:50 -0700

    error adding is not working

--- src/web/js/start_screen.js
+++ src/web/js/start_screen.js
@@ -1,21 +1,47 @@
-/*
-document.querySelector("#auth_form").addEventListener("submit",
-    function (event) {
-//    TODO validate token
-//    after validation, pass to excel uploader to validate and if errors then output the errors
-//    hand over the data in json from js to eel python and then to excel uploader
-        console.log("hello world!");
-        // console.log(event);
-        event.preventDefault();
-        return false;
+
+
+function getErrorElements() {
+    // get all error elements
+    const hostError = document.getElementById("host-error");
+    const apiTokenError = document.getElementById("api-token-error");
+    const projectNameError = document.getElementById("project-name-error");
+    const collectionNameError = document.getElementById("collection-name-error");
+    const excelFileError = document.getElementById("excel-file-error");
+    const dataPrivacyError = document.getElementById("data-privacy-error");
+
+    return {
+        "hostError": hostError,
+        "apiTokenError": apiTokenError,
+        "projectNameError": projectNameError,
+        "collectionNameError": collectionNameError,
+        "excelFileError": excelFileError,
+        "dataPrivacyError": dataPrivacyError,
     }
-);
-*/
+}
 
-function formValidation(host, apiToken, projectName, collectionName, isDataPublic, excelFile) {
-//    host validation
-    if (host === "") {
 
+
+
+function formValidation(userInput, errorElements) {
+
+    console.log(`userInput: ${userInput}, errors: ${errorElements}`)
+
+    console.log(`userInput: ${userInput[0]}`)
+
+//todo this method is not working
+    // loop through userInput json and error elements (both have exactly 5 elements)
+    // at the same time when there is an error in userInput then put up the error
+    // you can use the same pointer for both
+    for (let i = 0; (i < userInput.length && i < errorElements.length); i++) {
+
+        console.log(i)
+
+        if (userInput[i] === "") {
+            errorElements[i].className.remove("hidden");
+        }
+        else {
+            errorElements[i].className.add("hidden");
+        }
     }
 
 }
@@ -33,16 +59,17 @@ function submitForm(event) {
     const excelFile = document.getElementById("excel-file").files;
     console.log(excelFile);
 
-    // show validation errors
-    formValidation(host, apiToken, projectName, collectionName, isDataPublic, excelFile);
-
-
     const userInput = {
         "host": host,
         "api-token": apiToken,
         "project-name": projectName,
         "collection-name": collectionName,
-        "data-privacy-option": isDataPublic
+        "data-privacy-option": isDataPublic,
+        "excel-file": excelFile
     }
+
+    // show validation errors
+    formValidation(userInput, getErrorElements());
+
 
 }
