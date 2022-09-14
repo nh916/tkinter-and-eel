9272ca950a484a90c321441db8994c7ccc28d888
Navid Hariri <navidhariri25@gmail.com>
2022-09-14 12:46:37 -0700

    error_screen.html: using alerts as errors instead of plain text and good formatting of errors as well

--- src/main.py
+++ src/main.py
@@ -62,7 +62,7 @@ def read_excel_file(excel_file):
 eel.init("web")
 
 eel.start(
-    'templates/start_screen.html',
+    'templates/error_screen.html',
     jinja_templates='templates',
     size=(800, 850),
 )
